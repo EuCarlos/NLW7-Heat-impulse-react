d30dfdd982e22325f0957665a34c5cd566d0673d
Carlos Alves <carlosalves369m@hotmail.com>
2021-10-23 19:00:08 -0300

    Add Message Form

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -4,6 +4,8 @@ import { api } from '../../services/api'
 import styles from './styles.module.scss'
 import logoImg from '../../assets/logo.svg'
 
+import io from 'socket.io-client'
+
 type Message = {
     id: string, 
     text: string, 
@@ -13,10 +15,32 @@ type Message = {
     }
 }
 
+const messageQueue: Message[] = []
+
+const socket = io('http://localhost:8080')
+
+socket.on('new_message', (newMessage: Message) => { 
+    messageQueue.push(newMessage)
+})
+
 export function MessageList() {
 
 
     const [messages, setMessages] = useState<Message[]>([])
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if(messageQueue.length > 0) {
+                setMessages(prevState => [
+                    messageQueue[0],
+                    prevState[0],
+                    prevState[1]
+                ].filter(Boolean))
+
+                messageQueue.shift()
+            }
+        }, 3000)
+    })
 
 
     useEffect(() => {
--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -1,77 +1,87 @@
-import { createContext, ReactNode, useState } from "react";
-import { useEffect } from 'react'
+import { createContext, ReactNode, useEffect, useState } from "react";
 import { api } from "../services/api";
 
 type User = {
-    id: string, 
-    name: string, 
-    login: string, 
-    avatar_url: string
+  id: string;
+  name: string;
+  login: string;
+  avatar_url: string;
 }
 
 type AuthContextData = {
-    user: User | null,
-    signInUrl: string
+  user: User | null;
+  signInUrl: string;
+  signOut: () => void;
 }
 
 export const AuthContext = createContext({} as AuthContextData)
 
 type AuthProvider = {
-    children: ReactNode
+  children: ReactNode;
 }
 
 type AuthResponse = {
-    token: string,
-    user: {
-        id: string, 
-        avatar_url: string, 
-        name: string, 
-        login: string
-    } 
+  token: string;
+  user: {
+    id: string;
+    avatar_url: string;
+    name: string;
+    login: string;
+  }
 }
 
 export function AuthProvider(props: AuthProvider) {
-    const [user, setUser] = useState<User | null>(null)
-    const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=your_client_id`
+  const [user, setUser] = useState<User | null>(null)
 
-    async function signIn(githubCode: string) {
-        const response = await api.post<AuthResponse>('authenticate', {
-            code: githubCode
-        })
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=your_client_id`;
 
-        const { token, user } = response.data
+  async function signIn(githubCode: string) {
+    const response = await api.post<AuthResponse>('authenticate', {
+      code: githubCode,
+    })
 
-        localStorage.setItem('@dowhile:token', token)
+    const { token, user } = response.data
 
-        setUser(user)
+    localStorage.setItem('@dowhile:token', token)
+
+    api.defaults.headers.common.authorization = `Bearer ${token}`;
+
+    setUser(user)
+  }
+
+  function signOut() {
+    setUser(null)
+    localStorage.removeItem('@dowhile:token')
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('@dowhile:token')
+
+    if (token) {
+      api.defaults.headers.common.authorization = `Bearer ${token}`;
+
+      api.get<User>('profile').then(response => {
+        setUser(response.data)
+      })
     }
-    useEffect(() => {
-        const token = localStorage.getItem('@dowhile:token')
+  }, [])
 
-        if(token) {
-            api.defaults.headers.common.authorization = `Bearer ${token}`
-            
-            api.get('profile').then(response => {
-                console.log(response.data)
-            })
-        }
-    }, [])
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGithubCode = url.includes('?code=');
 
-    useEffect(() => {
-        const url = window.location.href
-        const hasGithubCode = url.includes('?code=')
+    if (hasGithubCode) {
+      const [urlWithoutCode, githubCode] = url.split('?code=')
 
-        if(hasGithubCode) {
-            const [ urlWithoutCode, githubCode] = url.split('?code=')
-            window.history.pushState({}, '', urlWithoutCode)
+      window.history.pushState({}, '', urlWithoutCode);
 
-            signIn(githubCode)
-        }
-    }, [])
+      signIn(githubCode)
+    }
+  }, [])
 
-    return (
-        <AuthContext.Provider value={{ signInUrl, user }}>
-            {props.children}
-        </AuthContext.Provider>
-    )
+  return (
+    <AuthContext.Provider value={{ signInUrl, user, signOut }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
 }
